d8a3e9b3403b64b9b7dee000667428716b4c88a7
Arekkusu <arekkusu5665@gmail.com>
2026-04-09 19:34:06 +0100

    Core funcionality added, considering moving off of OCR due to cpu bottlenecks

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use ocrs::{ImageSource, OcrEngine, OcrEngineParams};
 use rten::Model;
+use image_compare::{Algorithm};
 use image;
 
 use std::process::Command;
 use std::error::Error;
+use std::thread;
+use std::time::Duration;
 use envfile::EnvFile;
 #[allow(unused)]
 use std::path::Path;
@@ -33,9 +36,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     })?;
 
     loop {
-        let region1 = true; 
-        let ocr_input;
-
         let start = Instant::now();
         
         let screens = Screen::all().expect("Failed to find screen");
@@ -50,7 +50,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             screens.get(0).expect("No screens detected at all")
         });
 
-        let screenshot_image_path = file_path("images/test.png");
+        let screenshot_image_path = file_path("images/nintendo-switch-ui-screenshot.png");
 
         let width = screen.display_info.width as f32;
         let height = screen.display_info.height as f32;
@@ -65,39 +65,61 @@ fn main() -> Result<(), Box<dyn Error>> {
         image.save(&screenshot_image_path).unwrap();
         println!("运行耗时: {:?}", start.elapsed());
 
-        
+        let mut envfile = EnvFile::new(&file_path(".env")).unwrap();
+        let previous_game = envfile.get("CURRENT_GAME").unwrap_or("").to_string();
+
+        let switch_ui_reference_fullscreen = image::open("ui-reference/nintendo-switch-ui-fullscreen.png").unwrap().into_luma8();
+        let switch_ui_reference_windowed = image::open("ui-reference/nintendo-switch-ui-windowed.png").unwrap().into_luma8();
+        let switch_ui_screenshot = image::open("images/nintendo-switch-ui-screenshot.png").unwrap().into_luma8();
+
+        let ui_similarity_fullscreen = image_compare::gray_similarity_structure(&Algorithm::MSSIMSimple, &switch_ui_reference_fullscreen, &switch_ui_screenshot).unwrap();
+        let ui_similarity_windowed = image_compare::gray_similarity_structure(&Algorithm::MSSIMSimple, &switch_ui_reference_windowed, &switch_ui_screenshot).unwrap();
+
+        let score: f64 = f64::max(ui_similarity_fullscreen.score, ui_similarity_windowed.score);
+        let score_string = score.to_string();
+
+        let ocr_input;
         // Set image path and load the image to Ocr
-        if region1 == true {
-            let img_path = screenshot_image_path;
-            ocr_input = image_path(img_path, &engine)?;
+        if score > 0.9 {
+            if previous_game != "Home Menu" {
+                rm_env(); 
+                envfile.update("CURRENT_GAME", "Home Menu");
+                envfile.write().expect("Failed to write to dotenv file");
+                println!("{}", score_string);
+                execute();  
+            }
+                thread::sleep(Duration::from_millis(500));
+                continue;
         } else {
-            // Handle other regions or provide a default
-            // For now, let's assume we always have region1
-            continue;
+            let captured_image = screen.capture().unwrap();
+            captured_image.save(&screenshot_image_path).unwrap();
+            ocr_input = image_path(screenshot_image_path, &engine)?;
         }
 
         // Extract text from image using OcrEngine
         let extracted_text = engine.get_text(&ocr_input)?;
+        let detected_game = extracted_text.trim();
 
         println!("{}", extracted_text);
 
-        rm_env();
-        let mut envfile = EnvFile::new(&Path::new(".env")).unwrap();
-
         // Use match statement to pass game name to dotfile to be loaded into toml file
-        match extracted_text.as_str() {
-            "Mario Kart World" => {
+        match extracted_text.as_str().trim() {
+                "Mario Kart World" if previous_game != detected_game => {
+                rm_env();
                 envfile.update("CURRENT_GAME", "Mario Kart World");
                 envfile.write().expect("Failed to write to dotenv file");
                 execute();
-            }
-            "Pokemon Pokopia" => {
+                }
+            
+            "Pokemon Pokopia" if previous_game != detected_game => {
+                rm_env();
                 envfile.update("CURRENT_GAME", "Pokemon Pokopia");
                 envfile.write().expect("Failed to write to dotenv file");
                 execute();
             }
             _ => println!("Error: No game detected")
         }
+        thread::sleep(Duration::from_millis(500));
     }
 }
 
